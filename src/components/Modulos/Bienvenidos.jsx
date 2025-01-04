baebafc3e3f20c70fb94633004c133627db98111
Ivan <ivanmeyer1991@gmail.com>
2025-01-04 16:34:33 -0300

    eslint airbnb config

--- src/components/Modulos/Bienvenidos.jsx
+++ src/components/Modulos/Bienvenidos.jsx
@@ -1,14 +1,12 @@
-'use client'
+"use client";
 
-import { Playfair_Display } from 'next/font/google'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Playfair_Display as playfairDisplay } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
-const playfair = Playfair_Display({ subsets: ['latin'] })
+const playfair = playfairDisplay({ subsets: ["latin"] });
 
 export default function Bienvenidos() {
- 
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-50 px-4">
       <div className="max-w-md w-full text-center relative">
@@ -32,33 +30,29 @@ export default function Bienvenidos() {
               MICA
             </h1>
           </div>
-        <div className="w-full flex justify-center border-t-2">
-          <Image
-            src="/flores2.png?height=100&width=300"
-            alt="Decorative flowers bottom"
-            width={500}
-            height={100}
-            className="opacity-80 rotate-180"
-          />
-        </div>
+          <div className="w-full flex justify-center border-t-2">
+            <Image
+              src="/flores2.png?height=100&width=300"
+              alt="Decorative flowers bottom"
+              width={500}
+              height={100}
+              className="opacity-80 rotate-180"
+            />
+          </div>
           <p className="text-rose-300 text-sm mt-8">
             La música de fondo es parte de la experiencia
           </p>
           <div>
-          <Link
-            type="button"
-            href="/casamiento"
-            className="mt-6 px-8 py-2 bg-[#E5A19A] text-white rounded-md hover:bg-[#d8958e] transition-colors duration-300"
-          >
-            Ingresar
-          </Link>
+            <Link
+              type="button"
+              href="/casamiento"
+              className="mt-6 px-8 py-2 bg-[#E5A19A] text-white rounded-md hover:bg-[#d8958e] transition-colors duration-300"
+            >
+              Ingresar
+            </Link>
           </div>
         </div>
-
-      
       </div>
     </div>
-  )
+  );
 }
-
-
