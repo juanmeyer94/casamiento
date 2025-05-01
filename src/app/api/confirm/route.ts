e3bfac790bc92271095021a5748c50d0905b6afb
juanmeyer94 <juan_meyer@hotmail.com.ar>
2025-05-01 18:37:54 -0300

    new try...

--- src/app/api/confirm/route.ts
+++ src/app/api/confirm/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
-import appendToSheet from "../../../lib/sheets.ts";
+import { appendToSheet } from "../../../lib/sheets.ts";
 
-export default async function POST(req: Request) {
+export async function POST(req: Request) {
   const body = await req.json();
   const {
     name, email, attending, message,
