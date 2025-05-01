3919c859512fad3cc443f34dc03c902ec831720e
Juan Meyer <107147413+juanmeyer94@users.noreply.github.com>
2025-05-01 18:12:58 -0300

    Update DressTips.jsx
    Insisto con el eslint y la ptm

--- src/components/Modulos/DressTips.jsx
+++ src/components/Modulos/DressTips.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Image from "next/image";
 import { FolderIcon as ClothesHanger } from "lucide-react";
 
-
 export default function DressTips() {
   const [isOpen, setIsOpen] = useState(false);
 
